de057ffbc3cc0474cc906cea4bd55841fff065ed
Soham <sohammokal420@gmail.com>
2024-01-21 12:43:27 +0530

    First Push

---
+++ app/src/main/java/com/example/newschatbot/API.java
@@ -0,0 +1,6 @@
+package com.example.newschatbot;
+
+public class API {
+    public static String API_URL = "https://api.openai.com/v1/completions";
+    public static String API = "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";
+}
