fb62fa5753989600dbc2bef7c83ea65d5eb95948
Soham <sohammokal420@gmail.com>
2024-01-21 13:23:23 +0530

    Second Push

--- app/src/main/java/com/example/newschatbot/API.java
+++ app/src/main/java/com/example/newschatbot/API.java
@@ -2,5 +2,5 @@ package com.example.newschatbot;
 
 public class API {
     public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "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";
+    public static String API = "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";
 }
