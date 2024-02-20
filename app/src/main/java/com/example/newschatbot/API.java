810ca33ac0d19eeb52405a8a05b203a69f037c31
Soham <sohammokal420@gmail.com>
2024-02-20 20:46:48 +0530

    Payment Module Receipt Update

--- app/src/main/java/com/example/newschatbot/API.java
+++
@@ -1,6 +0,0 @@
-package com.example.newschatbot;
-
-public class API {
-    public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "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";
-}
